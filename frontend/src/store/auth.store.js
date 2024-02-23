faafeba74c7d010644fc740530b40acc9c4b01cf
Silali <gibsonsilali@gmail.com>
2024-02-23 08:27:42 +0300

    Creates auth store for global state mangement

---
+++ frontend/src/store/auth.store.js
@@ -0,0 +1,23 @@
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
+
+const useAuthStore = create(
+  persist((set, get) => ({
+    account: [],
+    userAccount: null,
+    ethBalance: 0,
+    isConnected: false,
+    updateUserDetails: ({ account, userAccount, ethBalance }) => {
+      return set({
+        account,
+        userAccount,
+        ethBalance: ethBalance.toString(),
+        isConnected: !!account,
+      });
+    },
+  }), {
+    name: 'userDetails'
+  }),
+);
+
+export default useAuthStore;
