270993db865a1721b7c14178a998fdad364f1d95
Silali <gibsonsilali@gmail.com>
2024-02-27 22:33:05 +0300

    removed debugging alert

--- frontend/src/components/shared/Navbar.jsx
+++ frontend/src/components/shared/Navbar.jsx
@@ -34,7 +34,6 @@ export const Navbar = () => {
 
   useEffect(() => {
     (async () => {
-      alert("Hello")
       await checkConnection(handleSuccessfulConnection, handleFailedConnection);
     })();
   }, []);
